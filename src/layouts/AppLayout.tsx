c9e549465478795d9c359f3f740c2d83272bf4d2
Diego Rojas <rrojas1296@gmail.com>
2026-01-08 17:29:33 -0500

    master: Edited height

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -6,7 +6,7 @@ const AppLayout = () => {
   return (
     <div className="lg:flex h-dvh">
       <Sidebar />
-      <div className="flex-1 z-10 overflow-y-auto custom-scroll">
+      <div className="flex-1 z-10 h-dvh overflow-y-auto custom-scroll">
         <Header />
         <div className="p-5 lg:p-6 text-text-1">
           <Outlet />
